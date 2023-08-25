71691f9c539d56ac3c0e9d4c7ba9cf7101d861c3
Abdullah Aziz <abdullahazees4@gmail.com>
2023-08-25 15:40:33 +0500

    code added

---
+++ src/contract/NFtMint.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NftMint is ERC721URIStorage,Ownable{
+    uint256 tokenId=0;
+    constructor()ERC721("ZeezNft","ZZNFT"){
+
+    }
+    function mint(address _to,string memory _tokenUri) public onlyOwner{
+_mint(_to,tokenId);
+_setTokenURI(tokenId,_tokenUri);
+        tokenId+=1;
+
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+const chainId = 11155111;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThirdwebProvider>
     <App />
-  </React.StrictMode>,
-)
+  </ThirdwebProvider>
+);
